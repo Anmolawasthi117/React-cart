efcf15b7dfc62bf386832302317ef6d04047a217
Anmol <anmolawasthi117@gmail.com>
2024-05-17 22:10:17 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+const img2 =
+  "https://imgs.search.brave.com/Z6L0NaUv_6NCUI-9APvx2Xwh8s0TyH7AKiq_2kzrgys/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTE5/MDQ0Nzg2NC9waG90/by9hcHBsZS1pcGhv/bmUtMTEtcHJvLWdy/YXktc21hcnRwaG9u/ZS5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9ekVUTEplZ3VM/b1RFRkJOS1BsMXZq/UFkxbHZQVzF1TTZH/UHB5aU1TdnNDMD0";
+
+const Home = () => {
+  const productList = [
+    {
+      name: "Mac Book",
+      price: 12000,
+      imgSrc: img1,
+      id: "asdhajsdbhjabhsjdfdfv",
+    },
+    {
+      name: "iphone",
+      price: 90000,
+      imgSrc: img2,
+      id: "sdjfdlaajsdbhjabhsjdfdfv",
+    },
+  ];
+
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (options) => {
+    dispatch({ type: "addToCart", payload: options });
+    dispatch({ type: "calculatePrice" });
+    toast.success("Added To Cart");
+  };
+  return (
+    <div className="home">
+      {productList.map((i) => (
+        <ProductCard
+          key={i.id}
+          imgSrc={i.imgSrc}
+          name={i.name}
+          price={i.price}
+          id={i.id}
+          handler={addToCartHandler}
+        />
+      ))}
+    </div>
+  );
+};
+
+const ProductCard = ({ name, id, price, handler, imgSrc }) => (
+  <div className="productCard">
+    <img src={imgSrc} alt={name} />
+    <p>{name}</p>
+    <h4>${price}</h4>
+    <button onClick={() => handler({ name, price, id, quantity: 1, imgSrc })}>
+      Add to Cart
+    </button>
+  </div>
+);
+
+export default Home;
